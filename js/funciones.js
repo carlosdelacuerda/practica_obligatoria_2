032a044fdafcb9a318961775becaa06c2119c01a
Carlos de la Cuerda <carlosdelacuerda@gmail.com>
2021-01-30 17:13:03 +0100

    Funcionalidad filtrar por prioridad terminada, no funcionas el event listener

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,5 @@
 
-
+//pintar tabla
 
 function pintarme (pNombre , pPrioridad) {
     listadoTareas.forEach(tarea => {
@@ -11,3 +11,46 @@ function pintarme (pNombre , pPrioridad) {
 
 pintarme()
 
+
+
+
+let option = document.querySelector('option')
+
+let selection = document.querySelector('select')
+
+
+function filtrado (event) {
+    alert()
+    switch (selection.value) {
+        case '0':
+            let todas = document.querySelectorAll('.alta , .media , .baja')
+            todas.forEach(borrar => {
+                borrar.style.display = 'block'
+            });
+            break
+        case '1':
+            let alta = document.querySelectorAll('.alta')
+            alta.forEach(borrar => {
+            borrar.style.display = 'none'
+            });
+            break
+        case '2':
+            let media = document.querySelectorAll('.media')
+            alta.forEach(borrar => {
+                borrar.style.display = 'none'
+            });
+            break
+        case '3':
+            let baja = document.querySelectorAll('.baja')
+            alta.forEach(borrar => {
+                borrar.style.display = 'none'
+            });
+            break        
+    }
+}
+
+
+
+
+selection.addEventListener('click', select )
+    
--- js/listadoTareas.js
+++ js/listadoTareas.js
@@ -11,5 +11,17 @@ let listadoTareas = new Array (
     {
         nombre: 'Hacer la compra',
         prioridad: 3
+    },
+    {
+        nombre: 'Sacar al perro',
+        prioridad: 1
+    },
+    {
+        nombre: 'Ir al WC',
+        prioridad: 2
+    },
+    {
+        nombre: 'Jugar a LOL',
+        prioridad: 3
     }
 )
