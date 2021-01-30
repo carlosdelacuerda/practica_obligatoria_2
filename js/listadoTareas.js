e4e4908b6dbbc219c6c57d9439207cd32b643ff6
Carlos de la Cuerda <carlosdelacuerda@gmail.com>
2021-01-30 10:09:00 +0100

    estructura HTML y listado precargado de tareas

---
+++ js/listadoTareas.js
@@ -0,0 +1,15 @@
+
+let listadoTareas = new Array (
+    {
+        nombre: 'Estudiar JS',
+        prioridad: 1
+    },
+    {
+        nombre: 'Sacar la basura',
+        prioridad: 2
+    },
+    {
+        nombre: 'Hacer la compra',
+        prioridad: 3
+    }
+)
